1ff123c3cb336bfa1e05177be90aa952ad91f664
Khalil <mohamed.khalil609@gmail.com>
2017-05-14 16:17:47 +0200

    add hash user password /users and before saving

--- playground/hashing.js
+++ playground/hashing.js
@@ -1,15 +1,29 @@
 const {SHA256} = require('crypto-js');
 const {sign, verify} = require('jsonwebtoken');
+var bcrypt = require('bcryptjs');
 
+var pass = '123abc!'
 
-var data = {
-  id: 10
-}
+bcrypt.genSalt(10, (err, salt) => {
+  bcrypt.hash(pass, salt, (err, hash) => {
+     console.log(hash);
+   });
+ });
 
-var token = sign(data, '123abc');
-console.log(token);
-var decoded = verify(token, '123abc');
-console.log('decoded', decoded);
+var hashedPass = '$2a$10$97sdXZMhHC5WwkBDH9hFT.47xn6yWuhkH09g8WFn/5.vLpZwSvjCi';
+
+bcrypt.compare(pass, hashedPass, (err, result)=> {
+  console.log(result);
+});
+
+// var data = {
+//   id: 10
+// }
+//
+// var token = sign(data, '123abc');
+// console.log(token);
+// var decoded = verify(token, '123abc');
+// console.log('decoded', decoded);
 //
 // var message = 'I am user number 3';
 //
